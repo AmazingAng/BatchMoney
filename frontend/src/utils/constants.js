09558129e3e5b627b1a2907e2fe2d4b67a1981db
Li, Amazing Ang <d0u.gie@hotmail.com>
2023-10-07 18:37:54 +0800

    add eth mainnet

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,11 @@
 export const supportedChains = [
   {
+    chainId: 1,
+    batchTransferAddress: "0xD35a289c1D5F6f6a604d6026111109694e51BA25",
+    blockExplorer: "https://etherscan.io/",
+    name: "eth mainnet",
+  },
+  {
     chainId: 5,
     batchTransferAddress: "0xDC7a1993196d63db926c3B2e1C42682f39885B96",
     blockExplorer: "https://goerli.etherscan.io/",
